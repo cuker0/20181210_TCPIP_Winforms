1dbedf4cde1c44fdc4610db3633fb1bd0e14bc05
Marcin Cukrowski <marcin.cukrowski@balluff.pl>
2018-12-10 15:01:50 +0100

    ZAD DOMOWE dodac obsluge bledow itp

--- 20181210_TCPIP_Winforms/Form1.Designer.cs
+++ 20181210_TCPIP_Winforms/Form1.Designer.cs
@@ -37,14 +37,18 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(276, 48);
+            this.button1.Location = new System.Drawing.Point(276, 101);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(125, 75);
+            this.button1.Size = new System.Drawing.Size(125, 22);
             this.button1.TabIndex = 0;
             this.button1.Text = "Send message";
             this.button1.UseVisualStyleBackColor = true;
@@ -59,7 +63,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(21, 159);
+            this.textBox2.Location = new System.Drawing.Point(21, 201);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
@@ -95,7 +99,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(21, 136);
+            this.label3.Location = new System.Drawing.Point(21, 181);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(120, 17);
             this.label3.TabIndex = 6;
@@ -122,11 +126,51 @@
             this.numericUpDown1.Size = new System.Drawing.Size(72, 22);
             this.numericUpDown1.TabIndex = 9;
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 130);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "SendHex";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(21, 151);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(231, 22);
+            this.textBox4.TabIndex = 11;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(276, 151);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Send";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(276, 49);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Connect";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(421, 389);
+            this.ClientSize = new System.Drawing.Size(457, 414);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -155,6 +199,10 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- 20181210_TCPIP_Winforms/Form1.cs
+++ 20181210_TCPIP_Winforms/Form1.cs
@@ -13,11 +13,14 @@ namespace _20181210_TCPIP_Winforms
 {
     public partial class Form1 : Form
     {
+        private TcpClient _tcpclient;
+
         public Form1()
         {
             InitializeComponent();
         }
-        public string Connect(String server, Int32 port, String message)
+
+        public string Connect(String server, Int32 port, byte[] message)
         {
             String responseData = String.Empty;
 
@@ -27,13 +30,12 @@ namespace _20181210_TCPIP_Winforms
                 // Note, for this client to work you need to have a TcpServer 
                 // connected to the same address as specified by the server, port
                 // combination.          
-              
-
-                TcpClient client = new TcpClient(server, port);
-                NetworkStream stream = client.GetStream();
+       
+               // TcpClient _tcpclient = new TcpClient(server, port);
+                NetworkStream stream = _tcpclient.GetStream();
 
                 // Translate the passed message into ASCII and store it as a Byte array.
-                byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+                byte[] data =message;
 
                 BCCHelper.CalculateAndAppendBcc(ref data);
 
@@ -52,8 +54,6 @@ namespace _20181210_TCPIP_Winforms
 
                 // String to store the response ASCII representation.
 
-               
-
                 // Read the first batch of the TcpServer response bytes.
 
                 Int32 bytes = stream.Read(data, 0, data.Length);
@@ -61,8 +61,8 @@ namespace _20181210_TCPIP_Winforms
                // Console.WriteLine("Received: {0}", responseData);
                  
                // Close everything.
-                stream.Close();
-                client.Close();
+              //  stream.Close();
+              //  _tcpclient.Close();
 
                
             }
@@ -78,7 +78,69 @@ namespace _20181210_TCPIP_Winforms
             return responseData;
         }
 
-       async Task<string> SendAsync(String server, Int32 port, String message)
+        public string Send(String server, Int32 port, byte[] message)
+        {
+            String responseData = String.Empty;
+
+            try
+            {
+                // Create a TcpClient.
+                // Note, for this client to work you need to have a TcpServer 
+                // connected to the same address as specified by the server, port
+                // combination.          
+
+
+                TcpClient client = new TcpClient(server, port);
+                NetworkStream stream = client.GetStream();
+
+                // Translate the passed message into ASCII and store it as a Byte array.
+                byte[] data = message;
+
+                BCCHelper.CalculateAndAppendBcc(ref data);
+
+                // Get a client stream for reading and writing.
+                //  Stream stream = client.GetStream();
+
+                // Send the message to the connected TcpServer. 
+                stream.Write(data, 0, data.Length); // czekamy na informacje zwrotna z odpowiedzia
+
+                //  Console.WriteLine("Sent: {0}", message);
+
+                // Receive the TcpServer.response.
+                // Buffer to store the response bytes.
+
+                data = new byte[256];
+
+                // String to store the response ASCII representation.
+
+
+
+                // Read the first batch of the TcpServer response bytes.
+
+                Int32 bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                // Console.WriteLine("Received: {0}", responseData);
+
+                // Close everything.
+                stream.Close();
+                client.Close();
+
+
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+
+            return responseData;
+        }
+
+
+        async Task<string> SendAsync(String server, Int32 port, String message)
         {
             String responseData = String.Empty;
 
@@ -140,11 +202,42 @@ namespace _20181210_TCPIP_Winforms
 
         private void button1_ClickAsync(object sender, EventArgs e)
         {
-            textBox2.Text= Connect(textBox3.Text, (int)numericUpDown1.Value, textBox1.Text);
+            byte[] bytesData = Encoding.ASCII.GetBytes(textBox1.Text);
+            textBox2.Text= Connect(textBox3.Text, (int)numericUpDown1.Value, bytesData);
             //textBox2.Text = reply.Result;
 
          //   string reply = SendAsync(textBox3.Text, (int)numericUpDown1.Value, textBox1.Text);
           //  textBox2.Text = reply;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] hexData = StringToByteArray(textBox4.Text);
+            textBox2.Text = Connect(textBox3.Text, (int)numericUpDown1.Value, hexData);
+        }
+
+        public static byte[] StringToByteArray(string hex)
+
+        {
+            return Enumerable.Range(0, hex.Length)
+                .Where(x => x % 2 == 0)
+                .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                .ToArray();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            _tcpclient = new TcpClient(textBox3.Text, (int)numericUpDown1.Value);
+
+            if (_tcpclient.Connected)
+            {
+                button3.BackColor = Color.Green;
+            }
+            else
+            {
+                button3.BackColor = Color.Red;
+            }
+            
         }
     }
 }
